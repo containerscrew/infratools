5d7bff4d7e36e17b8d7c79612c60f49d4153fd75
containerscrew <containerscrew@users.noreply.github.com>
2024-11-18 12:39:30 +0100

    Update run.sh script

--- run.sh
+++ run.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
 CONTAINER_NAME="infratools"
-CONTAINER_VERSION="v2.5.2"
+CONTAINER_VERSION="v2.6.0"
 
-if [ "$(docker ps | grep -c "${CONTAINER_NAME}")" -gt 0 ];then
+# Fetch latest image version (tag)
+LATEST_VERSION=$(curl -s "https://registry.hub.docker.com/v2/repositories/containerscrew/infratools/tags?page_size=1" | jq -r '.results[0].name')
+
+if [[ $? -ne 0 || -z "$LATEST_VERSION" ]]; then
+    echo -e "\e[31mError fetching the latest container version. Please ensure you have Internet access and that 'jq' is installed.\e[0m"
+fi
+
+if [[ "$CONTAINER_VERSION" != "$LATEST_VERSION" ]]; then
+    echo -e "\e[33m"
+    echo "###########################################################"
+    echo "---> Warning: The configured version (${CONTAINER_VERSION}) does not match the latest available version (${LATEST_VERSION})"
+    echo "---> Warning: Delete the current container and run again the script to pull the latest version"
+    echo "###########################################################"
+    echo -e "\e[0m"
+fi
+
+if [ $(docker ps --filter "name=^/${CONTAINER_NAME}$" --format '{{.Names}}' | wc -l) -gt 0 ]; then
     docker exec -ti ${CONTAINER_NAME} zsh
 else
     docker run -tid \
