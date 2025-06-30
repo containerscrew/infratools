27c2c55d4aaa175cb46a1c876e6f7de4a0b4d9a2
containerscrew <containerscrew@users.noreply.github.com>
2025-06-30 16:12:17 +0200

    Update run-infratools.sh

--- run-infratools.sh
+++ run-infratools.sh
@@ -2,7 +2,7 @@
 
 # Configuration
 CONTAINER_NAME="$(basename $PWD)"
-CONTAINER_VERSION="v2.9.3"
+CONTAINER_VERSION="v2.10.0"
 IMAGE_NAME="docker.io/containerscrew/infratools"
 REGISTRY_URL="https://registry.hub.docker.com/v2/repositories/containerscrew/infratools/tags?page_size=1"
 
