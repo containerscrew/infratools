a1d43d3d8104af7be2d1662112fe0e28ddd49ae4
containerscrew <containerscrew@users.noreply.github.com>
2026-05-14 08:43:46 +0200

    chore: update version in run-infratools

--- run-infratools.sh
+++ run-infratools.sh
@@ -2,7 +2,7 @@
 
 # Configuration
 CONTAINER_NAME="$(basename $PWD)"
-CONTAINER_VERSION="3.3.0"
+CONTAINER_VERSION="3.3.1"
 IMAGE_NAME="docker.io/containerscrew/infratools"
 REGISTRY_URL="https://registry.hub.docker.com/v2/repositories/containerscrew/infratools/tags?page_size=10"
 
