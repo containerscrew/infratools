c61baae900700fdeb7aeb7a3a68d0d0ba0ec0cd5
containerscrew <containerscrew@users.noreply.github.com>
2026-09-17 10:45:16 +0200

    Revert "feat: aws-vault"
    This reverts commit cc42fa1b1daf1a2228b0a1edfe9230b34886ffcc.

--- run-infratools.sh
+++ run-infratools.sh
@@ -7,18 +7,9 @@ CONTAINER_VERSION="3.4.0"
 IMAGE_NAME="docker.io/containerscrew/infratools"
 REGISTRY_URL="https://registry.hub.docker.com/v2/repositories/containerscrew/infratools/tags?page_size=10"
 
-# Optional aws-vault profile. When set (via -p <profile> or this environment
-# variable), the container receives short-lived credentials as environment
-# variables instead of a mounted ~/.aws directory.
-AWS_VAULT_PROFILE="${AWS_VAULT_PROFILE:-}"
-
 # Function to check prerequisites
 check_prerequisites() {
-    local required=(curl jq docker)
-    if [[ -n "$AWS_VAULT_PROFILE" ]]; then
-        required+=(aws-vault)
-    fi
-    for cmd in "${required[@]}"; do
+    for cmd in curl jq docker; do
         if ! command -v "$cmd" &>/dev/null; then
             echo -e "\e[31m[ERROR] Required command '$cmd' is not installed.\e[0m"
             exit 1
@@ -45,11 +36,6 @@ print_info() {
     echo "Configured version: ${CONTAINER_VERSION}"
     echo "Running version: ${CURRENT_RUNNING_VERSION}"
     echo "Latest version: ${LATEST_VERSION}"
-    if [[ -n "$AWS_VAULT_PROFILE" ]]; then
-        echo "AWS credentials: aws-vault profile ${AWS_VAULT_PROFILE}"
-    else
-        echo "AWS credentials: mounted ~/.aws"
-    fi
     echo "-----------------------------------------------------------"
 }
 
@@ -62,44 +48,20 @@ start_container() {
         ENV_FILE_OPTION=(--env-file "$ENV_FILE")
     fi
 
-    # Default: share the host AWS configuration with the container.
-    local AWS_OPTIONS=(-v ~/.aws:/home/infratools/.aws)
-    local AWS_VAULT_EXEC=()
-
-    if [[ -n "$AWS_VAULT_PROFILE" ]]; then
-        # aws-vault exports ephemeral credentials as AWS_* variables, so the
-        # container gets temporary keys instead of long-lived config files.
-        # A valueless '-e VAR' forwards the variable from the current environment.
-        AWS_OPTIONS=(
-            -e AWS_ACCESS_KEY_ID
-            -e AWS_SECRET_ACCESS_KEY
-            -e AWS_SESSION_TOKEN
-            -e AWS_SESSION_EXPIRATION
-        )
-        if [[ -n "$AWS_VAULT" ]]; then
-            # Nesting aws-vault fails with "running in an existing aws-vault
-            # subshell", so reuse the credentials already in the environment.
-            printf "\e[33m[WARNING] Already inside an aws-vault subshell (%s), reusing its credentials instead of profile '%s'.\e[0m\n" "$AWS_VAULT" "$AWS_VAULT_PROFILE"
-        else
-            AWS_VAULT_EXEC=(aws-vault exec "$AWS_VAULT_PROFILE" --)
-            printf "\e[32m[INFO] Using aws-vault profile '%s'\e[0m \n" "$AWS_VAULT_PROFILE"
-        fi
-    fi
-
     local CONTAINER_VERSION=${1:-$CONTAINER_LATEST_VERSION}
     printf "\e[32m[INFO] Starting a new container '${CONTAINER_NAME}'...\e[0m \n"
-    "${AWS_VAULT_EXEC[@]}" docker run -tid \
+    docker run -tid \
         --name "${CONTAINER_NAME}" \
         --rm \
         -h "${HOSTNAME}" \
         -v "$(pwd):/code" \
         -v ~/.ssh:/home/infratools/.ssh \
+        -v ~/.aws:/home/infratools/.aws \
         -v ~/.kube:/home/infratools/.kube \
-        "${AWS_OPTIONS[@]}" \
         "${EXTRA_VOLUMES[@]}" \
         -w /code/ \
         -e AWS_DEFAULT_REGION=eu-west-1 \
-        --dns 1.1.1.1 \
+        --dns 1.1.1.1 \ # Add your on-prem custom DNS servers here if needed
         "${ENV_FILE_OPTION[@]}" \
         "${IMAGE_NAME}:${CONTAINER_VERSION}"
 }
@@ -118,10 +80,6 @@ update_container() {
     docker exec -ti "${CONTAINER_NAME}" /bin/zsh
 }
 
-usage() {
-    printf "\e[32mUsage: $0 [-i (info)] [-u (update)] [-a (attach or create)] [-v <host_path>:<container_path>] [-p <aws-vault profile>]\e[0m\n"
-}
-
 # Fetch the current running version
 CURRENT_RUNNING_VERSION=$(docker ps --filter "name=^/${CONTAINER_NAME}$" --format '{{.Image}}' | awk -F':' '{print $2}')
 if [[ -z "$CURRENT_RUNNING_VERSION" ]]; then
@@ -131,43 +89,12 @@ fi
 # Fetch the latest version
 LATEST_VERSION=$(fetch_latest_version)
 
-# First pass: collect configuration options (-v, -p) regardless of the order
-# they were given in, so that '-a -v ...' and '-a -p ...' also work.
-EXTRA_VOLUMES=()
-while getopts "iuav:p:" opt; do
-    case "$opt" in
-        v)  # Add extra volume
-            if [[ "$OPTARG" == *:* ]]; then
-                EXTRA_VOLUMES+=("-v" "$OPTARG")
-            else
-                printf "\e[31m[ERROR] Invalid volume mapping. Use -v <host_path>:<container_path>\e[0m\n"
-                exit 1
-            fi
-            ;;
-        p)  # Use an aws-vault profile instead of mounting ~/.aws
-            AWS_VAULT_PROFILE="$OPTARG"
-            ;;
-        i|u|a) ;;  # Actions are handled in the second pass
-        *)  # Invalid option
-            printf "\e[31m[ERROR] Invalid option.\e[0m\n"
-            usage
-            exit 1
-            ;;
-    esac
-done
-
-# If no options are provided, show usage
-if [[ $OPTIND -eq 1 ]]; then
-    usage
-    exit 0
-fi
-
 # Check prerequisites
 check_prerequisites
 
-# Second pass: run the requested actions
-OPTIND=1
-while getopts "iuav:p:" opt; do
+# Parse options with getopts, including -v for extra volumes
+EXTRA_VOLUMES=()
+while getopts "iuav:" opt; do
     case "$opt" in
         i)  # Print info
             print_info
@@ -188,6 +115,22 @@ while getopts "iuav:p:" opt; do
                 attach_container
             fi
             ;;
-        v|p) ;;  # Already handled in the first pass
+        v)  # Add extra volume
+            if [[ "$OPTARG" == *:* ]]; then
+                EXTRA_VOLUMES+=("-v" "$OPTARG")
+            else
+                printf "\e[31m[ERROR] Invalid volume mapping. Use -v <host_path>:<container_path>\e[0m\n"
+                exit 1
+            fi
+            ;;
+        *)  # Invalid option
+            printf "\e[31m[ERROR] Invalid option. Use -i (info), -u (update), -a (attach), or -v <host_path>:<container_path>.\e[0m\n"
+            exit 1
+            ;;
     esac
 done
+
+# If no options are provided, show usage
+if [[ $OPTIND -eq 1 ]]; then
+    printf "\e[32mUsage: $0 [-i (info)] [-u (update)] [-a (attach or create)] [-v <host_path>:<container_path>]\e[0m\n"
+fi
